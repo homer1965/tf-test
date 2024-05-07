ddab5a0e3172c2eccfe68dd7ef462f717479727c
Rob Huijser <rob@huijser.nl>
2024-05-07 14:54:47 +0200

    delete all

--- main.tf
+++ main.tf
@@ -37,16 +37,16 @@ provider "aws" {
 
 # Add .gitignore file in this directory with the terraform.tfvars
 
-resource "aws_instance" "tc_instance" {
-  ami           = "ami-0c7c4e3c6b4941f0f"
-  instance_type = "t2.micro"
+# resource "aws_instance" "tc_instance" {
+#   ami           = "ami-0c7c4e3c6b4941f0f"
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "TC-triggered-instance"
-  }
-}
+#   tags = {
+#     Name = "TC-triggered-instance"
+#   }
+# }
 
-resource "aws_iam_user" "helen" {
-  name = "helen"
+# resource "aws_iam_user" "helen" {
+#   name = "helen"
   
-}
+# }
